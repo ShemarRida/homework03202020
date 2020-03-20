ec08da3340cf7482d7bdad848f3b2394e0f0f38c
Shemar Rida <shemarrida1227@gmail.com>
2020-03-20 17:44:27 -0400

    Added wethereyet

---
+++ WeThereYet.java
@@ -0,0 +1,11 @@
+package are;
+
+import java.util.Scanner;
+
+public class WeThereYet {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Are we there yet?");
+        String userAnswer = userInput.next();
+    }
+}
